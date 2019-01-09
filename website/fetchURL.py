7ad27bb415fb86432a4b84b90f4fea1655fa2f3c
cpruvost <cpruvost@HOME-PC>
2019-01-09 22:55:38 +0000

    ensure that it refreshes the cache after 12h

--- website/fetchURL.py
+++ website/fetchURL.py
@@ -157,9 +157,10 @@ def main():
           
         # checks if the cached file exist for this url and read it if it does
            
-        if os.path.isfile(theFile):
+        if os.path.isfile(theFile) and int(time.time()-creation_date(theFile)) < 43200: # more than 12h (43200s)
             #read the file
             if VERBOSE:
+                print(str(timestamp_to_datetime(creation_date(theFile))), str(timestamp_to_datetime(time.time())))
                 print("using cached version (" + theFile +") created: " + str(timestamp_to_datetime(creation_date(theFile))))
             
             processFile(theFile,siteName)
