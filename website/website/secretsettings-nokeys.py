c9c7a09085103f9852b69f1d7c25ef3819103c6b
PruvostC <PruvostC@ELSOXFL-7035551.science.regn.net>
2019-11-03 15:25:23 +0000

    added comments

--- website/website/secretsettings-nokeys.py
+++ website/website/secretsettings-nokeys.py
@@ -1,3 +1,3 @@
 # COPY of the file with secret data - with no sensitive data
-secret_k = '...........'
-secret_app_k = '...........'
+secret_k = '...........' #Django application secure key
+secret_app_k = '...........' #Google mail application key for mailing account
