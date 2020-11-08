6db71e02afe7dda9a40bb85f5651008f476b1724
PruvostC <PruvostC@ELSOXFL-100596.science.regn.net>
2020-11-08 21:00:23 +0000

    added first vue Forum App - will certainly need som eadjustments to stabilise this...

--- website/excellingyourself/urls.py
+++ website/excellingyourself/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('vueforum/', views.vueforum, name='forum'),
     path('contact', views.contact, name='contact'),
-]
+] + static(settings.FORUM_URL, document_root=settings.FORUM_ROOT)
