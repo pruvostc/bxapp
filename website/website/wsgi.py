8d7668bd1874eb2a0588e5317fc1f450d31927c7
PruvostC <PruvostC@ELSOXFL-100596.science.regn.net>
2020-11-11 08:33:28 +0000

    adjusted WSGI to reflect pythonanywhere settings

--- website/website/wsgi.py
+++ website/website/wsgi.py
@@ -8,10 +8,17 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
-
+# add your project directory to the sys.path
+project_home = u'/home/crispy/website'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+    
+# set environment variable to tell django where your settings.py is
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website.settings')
 os.environ.setdefault('RUNNING_ENV', 'prod')
 
+# serve django via WSGI
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
